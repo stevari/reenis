81e083df91e8b859d98243e7b9eece706eec63d8
stevari <villevartia@hotmail.com>
2022-04-16 14:14:33 +0300

    Koodia paranneltu hieman ja lisätty nappi jolla pääsee profiilin luonti näkymään.

--- app/src/main/java/com/northpolegames/reenisapp/ProfileActivity.java
+++ app/src/main/java/com/northpolegames/reenisapp/ProfileActivity.java
@@ -1,30 +1,35 @@
 package com.northpolegames.reenisapp;
+
 import java.util.ArrayList;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
     private ArrayList<Profile> profileList;
-    private Button registerButton;
     private Profile profiili;
+    private Button switchToCreateNewBtn;
+    private ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        registerButton = findViewById(R.id.registerButton); //Hakee id:n perusteella napin
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            profiili = new Profile("Ville",183,84);
-            profileList.add(profiili);
-            Log.d("testi",profiili.toString());
 
-            }
+        //Nappi joka vie käyttäjän edelliseen näkymään
+        findViewById(R.id.backBtn).setOnClickListener(view -> {
+            finish();
+        });
+
+
+        //Nappi joka vie käyttäjän näkymään, jossa voi luoda uuden profiilin
+        findViewById(R.id.switchToCreateNewBtn).setOnClickListener(view -> {
+            startActivity(new Intent(ProfileActivity.this, CreateProfileActivity.class));
         });
 
 
