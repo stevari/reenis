9b04f27afeceb6def76c3fc783b1c36eed7a4c38
Shatteri <elias.isopahkala@gmail.com>
2022-05-25 14:31:54 +0300

    *fragment_first.xml styles

--- app/src/main/java/com/northpolegames/reenisapp/FirstFragment.java
+++ app/src/main/java/com/northpolegames/reenisapp/FirstFragment.java
@@ -1,9 +1,11 @@
 package com.northpolegames.reenisapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +16,7 @@ import com.northpolegames.reenisapp.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private ImageButton back;
 
     @Override
     public View onCreateView(
@@ -25,6 +28,7 @@ public class FirstFragment extends Fragment {
         return binding.getRoot();
 
     }
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
